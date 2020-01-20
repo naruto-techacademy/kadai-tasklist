5718433338ad92b91147159852d9a90d3cac88bb
naruto.tech <naruto.techacademy@gmail.com>
2020-01-20 04:09:56 +0000

    add colum complete kadai

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -40,8 +40,14 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {   
+        $this->validate($request,[
+            'status' => 'required|max:10',
+            'content' => 'required|max:191',
+        ]);
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -87,7 +93,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request,[
+            'status' => 'required|max10',
+            'content' => 'required|max191',
+        ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
